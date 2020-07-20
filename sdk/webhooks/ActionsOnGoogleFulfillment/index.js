6679d0d770ceb955c6f83f459e142ef690c1cf9f
Khoa Truong <khoa.truong@u.northwestern.edu>
2020-07-20 11:04:48 -0500

    separate bus_num and bus_dir slot filling

--- sdk/webhooks/ActionsOnGoogleFulfillment/index.js
+++ sdk/webhooks/ActionsOnGoogleFulfillment/index.js
@@ -19,11 +19,10 @@ const functions = require('firebase-functions');
 
 const app = conversation({debug: true});
 
-app.handle('start_scene_initial_prompt', (conv) => {
-  console.log('Start scene: initial prompt');
+app.handle('lookup_bus_dir', (conv) => {
+  console.log('Start Scene: lookup bus dir');
   conv.overwrite = false;
-  conv.scene.next.name = 'actions.scene.END_CONVERSATION';
-  conv.add('Hello world from fulfillment');
+  conv.add('Which direction is your bus going?');
 });
 
 exports.ActionsOnGoogleFulfillment = functions.https.onRequest(app);
