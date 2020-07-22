c7baee54b9ffbfcc6a88d0a56831949c14c27c5b
Khoa Truong <khoa.truong@u.northwestern.edu>
2020-07-22 15:04:28 -0500

    add external api call
    -add axios package
    -lookup_bus_dir finds directions of the route or starts over if the route is invalid.

--- sdk/webhooks/ActionsOnGoogleFulfillment/index.js
+++ sdk/webhooks/ActionsOnGoogleFulfillment/index.js
@@ -16,14 +16,32 @@
 
 const {conversation} = require('@assistant/conversation');
 const functions = require('firebase-functions');
+const axios = require('axios');
 
 const app = conversation({debug: true});
+const API_KEY = '/?key=' + functions.config().ctabustracker.key;
+
+axios.defaults.baseURL = 'http://www.ctabustracker.com/bustime/api/v2';
 
 app.handle('lookup_bus_dir', (conv) => {
-  console.log('Start Scene: lookup bus dir');
-  console.log('By the way your CTA API is: ' + functions.config().ctabustracker.key);
-  conv.overwrite = false;
-  conv.add('Which direction is your bus going?');
+  let ROUTE = '&rt=' + conv.scene.slots['bus_num']['value'];
+  let API_PATH = '/getdirections' + API_KEY + ROUTE +'&format=json';
+
+  return axios.get(API_PATH)
+  .then(response => {
+    let directions = response.data['bustime-response'];
+    if (directions['error']){
+      conv.add('Route ' + directions['error'][0]['rt'] + ' does not exist. Let\'s start over and try another number.');
+      conv.scene.next.name= 'Start';
+      return Promise.resolve(response);
+    }
+    conv.overwrite = false;
+    conv.add('Are you ' + directions['directions'][0]['dir'] + ' or ' + directions['directions'][1]['dir'] + '?');
+    return Promise.resolve(response);
+    
+  }).catch(() => {
+    return Promise.reject(new Error('External API call rejected while handling lookup_bus_dir.'));
+  });
 });
 
 exports.ActionsOnGoogleFulfillment = functions.https.onRequest(app);
